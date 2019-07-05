bc28a502ae6b1e87d7e6186572f205d17cf41381
renanbr <renan@renandelima.com>
2019-07-05 10:11:53 +0100

    Replace Buzz with HttpClient

--- src/SymfonyCorp/Connect/Api/Api.php
+++ src/SymfonyCorp/Connect/Api/Api.php
@@ -11,12 +11,13 @@
 
 namespace SymfonyCorp\Connect\Api;
 
-use Buzz\Browser;
-use Buzz\Client\Curl;
-use GuzzleHttp\Psr7\Request;
-use Psr\Http\Message\ResponseInterface;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
+use Symfony\Contracts\HttpClient\ResponseInterface;
 use SymfonyCorp\Connect\Api\Parser\ParserInterface;
 use SymfonyCorp\Connect\Api\Parser\VndComSymfonyConnectXmlParser as Parser;
 use SymfonyCorp\Connect\Exception\ApiClientException;
@@ -32,15 +33,15 @@ class Api
 {
     const ENDPOINT = 'https://connect.symfony.com/api';
 
-    private $browser;
+    private $httpClient;
     private $parser;
     private $logger;
     private $endpoint;
     private $accessToken;
 
-    public function __construct($endpoint = null, Browser $browser = null, ParserInterface $parser = null, LoggerInterface $logger = null)
+    public function __construct($endpoint = null, HttpClientInterface $httpClient = null, ParserInterface $parser = null, LoggerInterface $logger = null)
     {
-        $this->browser = $browser ?: new Browser(new Curl());
+        $this->httpClient = $httpClient ?: HttpClient::create();
         $this->parser = $parser ?: new Parser();
         $this->endpoint = $endpoint ?: self::ENDPOINT;
         $this->logger = $logger ?: new NullLogger();
@@ -74,7 +75,9 @@ class Api
 
         $this->logger->info(sprintf('GET %s', $url));
 
-        return $this->processResponse($this->browser->sendRequest(new Request('GET', $url, array_merge($headers, $this->getAcceptHeader()))));
+        return $this->processResponse($this->httpClient->request('GET', $url, [
+            'headers' => array_merge($headers, $this->getAcceptHeader()),
+        ]));
     }
 
     public function submit($url, $method = 'POST', array $fields = [], $headers = [])
@@ -85,46 +88,68 @@ class Api
         $this->logger->debug(sprintf('Posted headers: %s', json_encode($headers)));
         $this->logger->debug(sprintf('Posted fields: %s', json_encode($fields)));
 
-        return $this->processResponse($this->browser->submitForm($url, $fields, $method, array_merge($headers, $this->getAcceptHeader())));
+        return $this->processResponse($this->httpClient->request($method, $url, [
+            'headers' => array_merge($headers, $this->getAcceptHeader()),
+            'body' => $fields,
+        ]));
     }
 
     private function processResponse(ResponseInterface $response)
     {
-        $this->logger->info('Response:'.$this->getStringResponse($response));
+        try {
+            $this->logger->info('Response:'.implode("\r\n", [
+                implode("\n", $response->getInfo('response_headers')),
+                $response->getContent(false),
+            ]));
 
-        if (500 <= $response->getStatusCode()) {
-            throw new ApiServerException($response->getStatusCode(), $response->getBody(), $response->getReasonPhrase(), $response->getHeaders());
-        }
-
-        if (400 <= $response->getStatusCode()) {
-            try {
-                $error = $this->parser->parse($response->getBody());
-                $error = $error instanceof Model\Error ? $error : new Model\Error();
-            } catch (ApiParserException $e) {
-                throw new ApiClientException($response->getStatusCode(), $response->getBody(), $response->getReasonPhrase(), $response->getHeaders(), null, $e);
+            if (204 === $response->getStatusCode()) {
+                return true;
             }
 
-            throw new ApiClientException($response->getStatusCode(), $response->getBody(), $response->getReasonPhrase(), $response->getHeaders(), $error);
+            $content = trim($response->getContent());
+            if (empty($content)) {
+                return true;
+            }
+
+            $object = $this->parser->parse($content);
+            $object->setApi($this);
+
+            return $object;
+        } catch (ServerExceptionInterface $exception) {
+            throw new ApiServerException(
+                $response->getStatusCode(false),
+                $response->getContent(false),
+                $exception->getMessage(),
+                $response->getHeaders(false)
+            );
+        } catch (ClientExceptionInterface $exception) {
+            try {
+                $error = $this->parser->parse($response->getContent(false));
+                $error = $error instanceof Model\Error ? $error : new Model\Error();
+            } catch (ApiParserException $e) {
+                throw new ApiClientException(
+                    $response->getStatusCode(),
+                    $response->getContent(false),
+                    $e->getMessage(),
+                    $response->getHeaders(false),
+                    null,
+                    $e
+                );
+            }
+
+            throw new ApiClientException(
+                $response->getStatusCode(false),
+                $response->getContent(false),
+                $exception->getMessage(),
+                $response->getHeaders(false),
+                $error
+            );
         }
-
-        if (204 === $response->getStatusCode()) {
-            return true;
-        }
-
-        $content = trim($response->getBody());
-        if (empty($content)) {
-            return true;
-        }
-
-        $object = $this->parser->parse($content);
-        $object->setApi($this);
-
-        return $object;
     }
 
     private function getAcceptHeader()
     {
-        return array('Accept: '.$this->parser->getContentType());
+        return ['Accept: '.$this->parser->getContentType()];
     }
 
     private function constructUrlWithAccessToken($url)
@@ -151,17 +176,5 @@ class Api
         }
 
         return $url;
-    }
-
-    private function getStringResponse(ResponseInterface $response)
-    {
-        $headers = $response->getHeaders();
-
-        $headers = array_map(function ($name, array $values) {
-            return sprintf('%s: %s', $name, implode(',', $values));
-        }, array_keys($headers), $headers);
-
-
-        return sprintf("\r\n%s\r\n%s\r\n", implode("\r\n", $headers), (string) $response->getBody());
     }
 }
