c9fc7e68423e9f5861130cd6d259b9af1039d482
Fabien Potencier <fabien@potencier.org>
2020-06-13 18:00:35 +0200

    Added Authenticator for Symfony 5.1+

---
+++ src/Security/ConnectPassport.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of the SymfonyConnect package.
+ *
+ * (c) Symfony <support@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SymfonyCorp\Connect\Security;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\PassportTrait;
+use Symfony\Component\Security\Http\Authenticator\Passport\UserPassportInterface;
+use SymfonyCorp\Connect\Api\Entity\User;
+
+/**
+ * @author Fabien Potencier <fabien@symfony.com>
+ */
+class ConnectPassport implements UserPassportInterface
+{
+    use PassportTrait;
+
+    protected $user;
+
+    private $apiUser;
+    private $accessToken;
+    private $scope;
+
+    /**
+     * @param BadgeInterface[] $badges
+     */
+    public function __construct(UserInterface $user, User $apiUser, string $accessToken, string $scope, array $badges = [])
+    {
+        $this->user = $user;
+        $this->apiUser = $apiUser;
+        $this->accessToken = $accessToken;
+        $this->scope = $scope;
+
+        foreach ($badges as $badge) {
+            $this->addBadge($badge);
+        }
+    }
+
+    public function getUser(): UserInterface
+    {
+        return $this->user;
+    }
+
+    public function getApiUser(): User
+    {
+        return $this->apiUser;
+    }
+
+    public function getAccessToken(): string
+    {
+        return $this->accessToken;
+    }
+
+    public function getScope(): string
+    {
+        return $this->scope;
+    }
+}
