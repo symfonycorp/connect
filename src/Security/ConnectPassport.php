232c5683556d189d20b2b1cf375af04cc1dc8313
Javier Eguiluz <javier.eguiluz@gmail.com>
2022-01-04 10:18:04 +0100

    Fix ConnectPassport for Symfony 5.4

--- src/Security/ConnectPassport.php
+++ src/Security/ConnectPassport.php
@@ -12,6 +12,7 @@
 namespace SymfonyCorp\Connect\Security;
 
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\PassportTrait;
 use Symfony\Component\Security\Http\Authenticator\Passport\UserPassportInterface;
 use SymfonyCorp\Connect\Api\Entity\User;
@@ -19,7 +20,7 @@ use SymfonyCorp\Connect\Api\Entity\User;
 /**
  * @author Fabien Potencier <fabien@symfony.com>
  */
-class ConnectPassport implements UserPassportInterface
+class ConnectPassport extends Passport implements UserPassportInterface
 {
     use PassportTrait;
 
