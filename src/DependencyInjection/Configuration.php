cba65c75e1f1b7428b1544a9ef2a540edb6ab3a9
Javier Eguiluz <javier.eguiluz@gmail.com>
2022-02-23 17:37:46 +0100

    -

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -19,7 +19,7 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
  */
 class Configuration implements ConfigurationInterface
 {
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('symfony_connect');
         $rootNode = $treeBuilder->getRootNode();
