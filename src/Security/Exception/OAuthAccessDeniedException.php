23f3cc30153443f11cb3a228b8e64abb34700af9
Javier Eguiluz <javier.eguiluz@gmail.com>
2023-12-27 09:56:09 +0100

    Add some return types to AuthenticationException methods

--- src/Security/Exception/OAuthAccessDeniedException.php
+++ src/Security/Exception/OAuthAccessDeniedException.php
@@ -4,7 +4,7 @@ namespace SymfonyCorp\Connect\Security\Exception;
 
 class OAuthAccessDeniedException extends AuthenticationException
 {
-    public function getMessageKey()
+    public function getMessageKey(): string
     {
         return 'You denied access to your SymfonyConnect account';
     }
